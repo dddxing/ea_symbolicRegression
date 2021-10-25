b5367ce493e7f935611059f76d4982be1871330d
Daming Xing <dx2222@columbia.edu>
2021-10-25 16:01:17 -0400

    tree graphics, plots

--- graphics/main.py
+++ graphics/main.py
@@ -1,6 +1,10 @@
 from graphics import *
+import math
+import random
 
-filename = "test_equ.txt"
+filename = "ea_dm_equation.txt"
+levels = [512, 256, 128, 64, 32, 16, 8, 4, 2, 1]
+
 def read_equation(filename):
     equations = []
     with open(f'{filename}', 'r') as f:
@@ -10,18 +14,56 @@ def read_equation(filename):
 
     return equations
 
+
+def build_tree(binary_heap, win):
+
+    ptr = 1
+    radius = 20
+
+    locations = [[0, 0], [2500, 50]]
+
+    print(locations[ptr][0])
+    print(locations[ptr][1])
+
+    while ptr < len(binary_heap):
+        x = locations[ptr][0]
+        y = locations[ptr][1]
+        # print(f"x = {x}, y = {y}")
+        pt = Point(x, y)
+        cir = Circle(pt, radius)
+        txt = Text(pt, binary_heap[ptr])
+
+        level = math.floor(math.log2(ptr))
+
+        print(binary_heap[ptr], level)
+        cir.draw(win)
+        txt.draw(win)
+
+        child_lt = [x - (1 * levels[level]), y + 100]
+        child_rt = [x + (1 * levels[level]), y + 100]
+
+        if ptr < len(binary_heap) // 2:
+            line_lt = Line(Point(x,y+radius), Point(child_lt[0], child_lt[1]-radius))
+            line_rt = Line(Point(x,y+radius), Point(child_rt[0], child_rt[1]-radius))
+
+            line_lt.draw(win)
+            line_rt.draw(win)
+
+        locations.append(child_lt)
+        locations.append(child_rt)
+
+        ptr += 1
+    # print(len(locations))
+
 def main():
-    win = GraphWin("My window", 500, 500)
+    win = GraphWin("My window", 5000, 2000)
 
     win.setBackground(color_rgb(255, 255, 255))
-
-    equations = read_equation(filename=filename)
-
-    for i in range(1, len(equations)):
-        pt = Point()
+    bh = read_equation(filename=filename)
+    build_tree(bh, win=win)
 
     win.getMouse()
     win.close()
 
-# main()
-print(read_equation(filename))
+
+main()
